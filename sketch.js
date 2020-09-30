4e188a58bf1e30df679b4642010d7881070f1116
Raghavkhandelwal7 <71634967+Raghavkhandelwal7@users.noreply.github.com>
2020-09-30 08:32:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,12 @@
-var fixedrect,movingrect;
+var car,wall;
+var speed,weight;
+
 function setup() {
   createCanvas(800,400);
- fixedrect= createSprite(400, 200, 50, 80);
- movingrect= createSprite(300,300,80,50);
- fixedrect.shapeColor="green";
- movingrect.shapeColor="green";
+  createSprite(400, 200, 50, 50);
 }
 
 function draw() {
-  background(255,255,255); 
-  movingrect.x=World.mouseX;
-  movingrect.y=World.mouseY;
-if (movingrect.x-fixedrect.x<=movingrect.width/2+fixedrect.width/2&&
-  fixedrect.x-movingrect.x<=movingrect.width/2+fixedrect.width/2&&
-  movingrect.y-fixedrect.y<=movingrect.height/2+fixedrect.height/2&&
-  fixedrect.y-movingrect.y<=movingrect.height/2+fixedrect.height/2) {
-  fixedrect.shapeColor="red";
- movingrect.shapeColor="red";
-} else {
-  fixedrect.shapeColor="green";
-  movingrect.shapeColor="green"; 
-}
+  background(255,255,255);  
   drawSprites();
 }
